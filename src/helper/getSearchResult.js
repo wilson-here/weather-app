fd603f56aecb92aec3ca2cef159edc547739f541
quanganhha99@gmail.com <quanganhha99@gmail.com>
2023-12-17 02:37:02 +0700

    fix mixed content

--- src/helper/getSearchResult.js
+++ src/helper/getSearchResult.js
@@ -1,6 +1,6 @@
 export const getSearchResult = async (val) => {
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const apiUrl = `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${val}`;
+  const apiUrl = `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${val}`;
 
   const header = { mode: "cors" };
 
--- src/helper/getWeather.js
+++ src/helper/getWeather.js
@@ -1,6 +1,6 @@
 export const getWeather = (val) => {
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${val}`;
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${val}`;
 
   const header = { mode: "cors" };
 
